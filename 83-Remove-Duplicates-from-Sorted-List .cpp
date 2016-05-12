f5160cf782e2ce6924aa05056983ce29e0b699cc
JochenZou <zouquanchen@126.com>
2016-05-12 20:43:05 +0800

    Update 83-Remove-Duplicates-from-Sorted-List .cpp

--- 83-Remove-Duplicates-from-Sorted-List .cpp
+++ 83-Remove-Duplicates-from-Sorted-List .cpp
@@ -1,4 +1,14 @@
 /**
+ * Questioin:
+ * Given a sorted linked list, delete all duplicates such that each element appear only once.
+ * For example,
+ * Given 1->1->2, return 1->2.
+ * Given 1->1->2->3->3, return 1->2->3.
+*/
+
+
+
+/**
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
